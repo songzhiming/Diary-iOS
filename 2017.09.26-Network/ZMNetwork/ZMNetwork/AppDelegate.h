cbd0786f15ea35ba1d9566e8aaa3d1d1fb6f30ba
zhiming9 <zhiming9@staff.sina.com.cn>
2017-09-26 15:07:42 +0800

    初始化项目

---
+++ 2017.09.26-Network/ZMNetwork/ZMNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZMNetwork
+//
+//  Created by zhiming9 on 2017/9/26.
+//  Copyright © 2017年 zhiming9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
